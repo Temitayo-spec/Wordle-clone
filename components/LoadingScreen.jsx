6b8ac6b07dfc0128086805fa3830e1e32e84c8c2
unknown <olawanletemitayo@gmail.com>
2022-08-26 18:52:00 +0100

    added a loading screen with animations

--- components/LoadingScreen.jsx
+++ components/LoadingScreen.jsx
@@ -1,16 +1,17 @@
-import React from "react";
-import styles from "../styles/loadingScreen.module.css";
+/* eslint-disable linebreak-style */
+import React from 'react';
+import styles from '../styles/loadingScreen.module.css';
 
-const LoadingScreen = () => {
-  return (
-    <div className={styles.screen}>
-      <div className={styles.balls}>
-        <div className={`${styles.ball} ${styles.one}`}></div>
-        <div className={`${styles.ball} ${styles.two}`}></div>
-        <div className={`${styles.ball} ${styles.three}`}></div>
-      </div>
+const LoadingScreen = () => (
+  <div className={styles.screen}>
+    <div className={styles.balls}>
+      <div className={`${styles.ball} ${styles.one}`} />
+      <div className={`${styles.ball} ${styles.two}`} />
+      <div className={`${styles.ball} ${styles.three}`} />
     </div>
-  );
-};
+
+    <h1>Wordle</h1>
+  </div>
+);
 
 export default LoadingScreen;
--- pages/_app.js
+++ pages/_app.js
@@ -1,29 +1,35 @@
 /* eslint-disable quotes */
 /* eslint-disable react/jsx-props-no-spreading */
-// import { useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 import { Provider } from "react-redux";
-// import LoadingScreen from "../components/LoadingScreen";
+import LoadingScreen from "../components/LoadingScreen";
 import Default from "../layouts/default";
 import store from "../store";
 import "../styles/global.css";
 
 function MyApp({ Component, pageProps }) {
-  // const [time, setTime] = useState(true);
+  const [time, setTime] = useState(true);
 
-  // useEffect(() => {
-  //   setTimeout(() => {
-  //     setTime(false);
-  //   }, 3000);
-  // }, []);
+  useEffect(() => {
+    setTimeout(() => {
+      setTime(false);
+    }, 3000);
+  }, []);
 
   return (
-    <div className="layouts-ctn">
-      <Provider store={store}>
-        <Default>
-          <Component {...pageProps} />
-        </Default>
-      </Provider>
-    </div>
+    <>
+      {time ? (
+        <LoadingScreen />
+      ) : (
+        <div className="layouts-ctn">
+          <Provider store={store}>
+            <Default>
+              <Component {...pageProps} />
+            </Default>
+          </Provider>
+        </div>
+      )}
+    </>
   );
 }
 
